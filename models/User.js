e4c94a8e561d45b5ce841178dc71a05ee22a8120
Kevin Cauto <kevinmcauto@gmail.com>
2018-01-05 16:23:59 -0500

    Noticed that logging out seems to be an issue in production.  Made some changes.

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    googleId: String
+  googleId: String
 });
 
 mongoose.model('users', userSchema);
