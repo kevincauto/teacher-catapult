775df9498d08d83a74cecd52d20a31cc4275d3da
Kevin Cauto <kevinmcauto@gmail.com>
2018-01-15 21:55:43 -0500

    Random interview question generator added.

--- client/src/components/Interview.js
+++ client/src/components/Interview.js
@@ -2,6 +2,141 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Interview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      q1: '',
+      q2: '',
+      q3: '',
+      q4: '',
+      q5: '',
+      q6: '',
+      q7: '',
+      q8: ''
+    };
+
+    this.generateQuestions = this.generateQuestions.bind(this);
+  }
+
+  componentDidMount() {
+    this.generateQuestions();
+  }
+
+  generateQuestions() {
+    const firstQuestions = [
+      'Why are you a good fit for this job and our school district?',
+      'What makes you qualified for this teaching position?',
+      'Tell us a little about your professional background.',
+      'Can you tell us what separates you from other teaching candidates?'
+    ];
+
+    const secondQuestions = [
+      'Why did you become a teacher?',
+      'What is your philosophy on teaching?',
+      'Where do you see yourself in ten years?',
+      'What is the most satisfying thing about teaching?',
+      'What are your three greatest strengths?',
+      'What part of teaching do you look the most forward to?'
+    ];
+
+    const thirdQuestions = [
+      'Name three of your weaknesses.',
+      'Name five words that describe you.',
+      "What would you do if you received a note from a parent asking for their son to be excused from last night's homework because the student was too busy with another activity?",
+      'What is the biggest challenge in teaching?',
+      'What is the scariest thing about teaching?',
+      'What are your interests outside of teaching?',
+      'What are the important aspects of a good principal?',
+      'What type of in-service topics would you be most interested in?',
+      'What is the difference between a good teacher and an outstanding teacher?',
+      'Are you a flexible person?',
+      'What would your last boss say about you?',
+      'What is the last book you read? When did you read it? Tell us about it.',
+      'What is your least favorite subject, and age group, to teach?',
+      'What do you feel is wrong with public education?',
+      'How would one of your students describe you?',
+      'Imagine you are at your retirement party at the end of your career. How would people describe you as a teacher?',
+      'If you were a fruit, which fruit would you be and why?',
+      'What type of student were you in high school?',
+      'Do you want students to like you? Why or why not?',
+      'Who do you look up to and want to emulate?'
+    ];
+
+    const fourthQuestions = [
+      'Recall a time when things went poorly during a lesson.  How did you deal with the issue?',
+      'What is your classroom management plan and what do you hope to accomplish with it?',
+      'What does a model classroom look like to you?',
+      'How do you avoid misbehavior in your classroom?',
+      'What was the most difficult child you have ever dealt with, and how did you deal with them?',
+      'Would you create a behavior modification plan for ongoing misbehavior?',
+      'What is the most challenging behavioral situation you have ever dealt with? How did you react?',
+      'What are your classroom rules?'
+    ];
+
+    const fifthQuestions = [
+      'Would you say that you are a tough teacher?',
+      'Describe an example of when you used positive reinforcement.',
+      'How do you integrate technology into your lessons?',
+      'How do you allow students to express their creativity in your classroom?',
+      'Do you have students use higher order thinking in your class? Give an example.',
+      'How do you develop self-esteem within students?',
+      'How do you prepare students for standardized testing?',
+      'How do you make learning fun?',
+      'Describe a typical lesson.',
+      'What are techniques you use to teach besides direct instruction?',
+      'What do you do if the whole class is "not getting it"?',
+      'How closely do you follow your lesson plan?',
+      'What do you put in your learning objectives of your lesson plan?',
+      'How do you incorporate writing into your lessons?',
+      'Describe the most effective teaching techniques.',
+      'How do you connect your lessons to the "real world"?',
+      'Is it ok for a classroom to be noisy?',
+      'How much homework do you assign and how often do you assign it? How do you know this is a good amount?',
+      'How do you stay current in your field?'
+    ];
+
+    const sixthQuestions = [
+      'Give an example of how you differentiated instruction in a lesson.',
+      'How do you accommodate for a gifted student in your class?',
+      'How do you manage students with different reading abilities?',
+      'Explain how you meet IEP needs.',
+      'How important is differentiating instruction to you?',
+      'How do you feel about inclusive classrooms?',
+      'What is your experience co-teaching with an inclusion specialist?',
+      'How do you accommodate for non-English speakers?'
+    ];
+
+    const seventhQuestions = [
+      'Give us examples of how you communicated with other teachers in your department?',
+      'Give us an example of effective communication with an administrator.',
+      'Have you ever utilized a class newsletter? What did you include in the letter?',
+      'How would you deal with an angry parent if they called you?',
+      'How do you communicate with parents on a regular basis?',
+      "How do you keep parents aware of their son's or daughter's grades?",
+      'How would you react if a parent complained about your class?',
+      "What would you tell a parent if he or she was concerned about their kid's grades?",
+      'What course of action would you take if a student says he or she is being abused at home?'
+    ];
+
+    const eigthQuestions = [
+      'Is there anything we have not talked about that you would like to share with us?',
+      'Do you have any questions for us?'
+    ];
+
+    let stateQuestions = {
+      q1: firstQuestions[Math.floor(Math.random() * firstQuestions.length)],
+      q2: secondQuestions[Math.floor(Math.random() * secondQuestions.length)],
+      q3: thirdQuestions[Math.floor(Math.random() * thirdQuestions.length)],
+      q4: fourthQuestions[Math.floor(Math.random() * fourthQuestions.length)],
+      q5: fifthQuestions[Math.floor(Math.random() * fifthQuestions.length)],
+      q6: sixthQuestions[Math.floor(Math.random() * sixthQuestions.length)],
+      q7: seventhQuestions[Math.floor(Math.random() * seventhQuestions.length)],
+      q8: eigthQuestions[Math.floor(Math.random() * eigthQuestions.length)]
+    };
+
+    this.setState(stateQuestions);
+  }
+
   render() {
     return (
       <div className="container">
@@ -15,16 +150,20 @@ class Interview extends Component {
           <div className="row">
             <div className="col-md-8 col-lg-9">
               <center>
-                <h1>88 Teacher Interview Questions</h1>
+                <h1>
+                  Teacher Interview Question Guide: Questions and How to Answer
+                </h1>
               </center>
-              <div class="well well-lg">
+              <div className="well well-lg">
                 <strong>Table of Contents</strong>
                 <br />
                 <a href="#88">88 Common Teacher Interview Questions</a>
                 <br />
-                <a href="#">Practice Interview Generator (With Answers!)</a>
+                <a href="#generator">
+                  Practice Interview Generator (With Answer Hints!)
+                </a>
               </div>
-
+              <h2 className="text-center">88 Teacher Interview Questions</h2>
               <p>
                 <a name="88" />
                 <br />
@@ -37,7 +176,7 @@ class Interview extends Component {
                 that you can prepare for it just as you would for an important
                 test.
               </p>
-              <h2>The Introduction Questions</h2>
+              <h3>The Introduction Questions</h3>
               <ol>
                 <li>
                   Why are you a good fit for this job and our school district?
@@ -53,7 +192,7 @@ class Interview extends Component {
                 <li>What is your philosophy on teaching?</li>
                 <li>What separates you from other teaching candidates?</li>
               </ol>
-              <h2>Questions About College and Student Teaching</h2>
+              <h3>Questions About College and Student Teaching</h3>
               <ol>
                 <li>How well has your university prepared you for teaching?</li>
                 <li>What was the most useful college course you have taken?</li>
@@ -67,7 +206,7 @@ class Interview extends Component {
                 </li>
                 <li>Describe your student teaching experience.</li>
               </ol>
-              <h2>Personal Information</h2>
+              <h3>Personal Information</h3>
               <ol>
                 <li>Where do you see yourself in ten years?</li>
                 <li>What are your three greatest strengths?</li>
@@ -130,7 +269,7 @@ class Interview extends Component {
                 <li>Do you want students to like you? Why or why not?</li>
                 <li>Who do you look up to and want to emulate?</li>
               </ol>
-              <h2>Interview Questions About Teaching Style</h2>
+              <h3>Interview Questions About Teaching Style</h3>
               <ol>
                 <li>Would you say that you are a tough teacher?</li>
                 <li>
@@ -170,7 +309,7 @@ class Interview extends Component {
                 </li>
                 <li>How do you stay current in your field?</li>
               </ol>
-              <h2>Interview Questions About Teacher Communication</h2>
+              <h3>Interview Questions About Teacher Communication</h3>
               <ol>
                 <li>
                   Give us examples of how you communicated with other teachers
@@ -209,9 +348,9 @@ class Interview extends Component {
                   she is being abused at home?
                 </li>
               </ol>
-              <h2>
+              <h3>
                 Teacher Interview Questions About Differentiating Instruction
-              </h2>
+              </h3>
               <ol>
                 <li>
                   Give an example of how you differentiated instruction in a
@@ -231,7 +370,7 @@ class Interview extends Component {
                 </li>
                 <li>How do you accommodate for non-English speakers?</li>
               </ol>
-              <h2>Questions About Classroom Management</h2>
+              <h3>Questions About Classroom Management</h3>
               <ol>
                 <li>
                   What is your classroom management plan and what do you hope to
@@ -252,7 +391,7 @@ class Interview extends Component {
                 </li>
                 <li>What are your classroom rules?</li>
               </ol>
-              <h2>The Closing Interview Questions</h2>
+              <h3>The Closing Interview Questions</h3>
               <ol>
                 <li>
                   Is there anything we have not talked about that you would like
@@ -260,6 +399,152 @@ class Interview extends Component {
                 </li>
                 <li>Do you have any questions for us?</li>
               </ol>
+              <br />
+
+              <a name="generator" />
+              <h2 className="text-center">
+                Random Teacher Interview Generator
+              </h2>
+
+              <p>
+                Below you will find a randomly generated teacher interview. You
+                can press the button below to randomly generate a new interview
+                whenever you want. (There are over a million unique interviews
+                that can be generated.) Below each question you will find a hint
+                that you can reveal if you choose.
+              </p>
+
+              <p>
+                <strong>Consider answering these questions out loud</strong>, so
+                that you can truly verbalize your thoughts and simulate a real
+                interview. If you have a willing partner, have someone else read
+                you the questions one-by-one to truly simulate the interview
+                experience.
+              </p>
+              <div className="text-center">
+                <button
+                  className="btn btn-primary"
+                  onClick={this.generateQuestions}
+                >
+                  Generate a Random Set of Interview Questions!
+                </button>
+              </div>
+              <h3>Question #1:</h3>
+              <div>
+                <strong>{this.state.q1}</strong>
+              </div>
+              <br />
+              <div className="well well-lg">
+                <strong>Hint:</strong> Have a 30-second to 2-minute
+                summaryspeech about yourself prepared beforehand. Think about
+                this as a commercial about yourself. Consider what makes you a
+                great candidate and what makes you different from other teachers
+                out there. Let your interviewers know what sets you apart from
+                the rest of the pack.
+              </div>
+              <h3>Question #2:</h3>
+              <div>
+                <strong>{this.state.q2}</strong> <br />
+              </div>
+              <br />
+              <div className="well well-lg">
+                <strong>Hint:</strong> The answer to this question will be
+                personal to you. Try to work in phrases like "I'm passionate
+                about helpingstudents/children and I find teaching an incredibly
+                rewarding experience." AVOID saying things like "I'm in it for
+                the paycheck and love my summers off."
+              </div>
+              <h3>Question #3:</h3>
+              <div>
+                <strong>{this.state.q3}</strong>
+              </div>
+              <br />
+              <div className="well well-lg">
+                <strong>Hint:</strong> Again, the answer to this will be
+                personal to you, but keep in mind that you may run into
+                questions that you may not be prepared for. Sometimes you will
+                get questions that are meant to "throw you off" a bit. Some are
+                easier than others to improvise with an answer. Just stay calm
+                and answer this question as best as possible. This may be
+                testing to see how you react to unexpected things.
+              </div>
+              <h3>Question #4:</h3>
+              <div>
+                <strong>{this.state.q4}</strong>
+              </div>
+              <br />
+              <div className="well well-lg">
+                <strong>Hint:</strong> It is common to get a question about
+                classroom management and behavior. You should have an anecdotal
+                account of how you turned a bad situation into a positive one.
+                Explain that teachers should always have a backup plan and that
+                they should expect the unexpected. Also, make sure you note that
+                classroom management begins with your own preparation and lesson
+                plan. An engaging lesson, means engaged students and engaged
+                student leaves them no time to misbehave.
+              </div>
+              <h3>Question #5:</h3>
+              <div>
+                <strong>{this.state.q5}</strong>
+              </div>
+              <br />
+              <div className="well well-lg">
+                <strong>Hint:</strong> This question refers to your teaching
+                style. Think back to your teaching experiences or student
+                teaching experiences. Think about what works best for you and
+                what an ideal lesson would look like. In a perfect world, you
+                want to be able to describe lesson that is fun, creative,
+                connects to the real world, adheres to standards, includes a
+                check for learning, and utilizes technology.
+              </div>
+              <h3>Question #6:</h3>
+              <div>
+                <strong>{this.state.q6}</strong>
+              </div>
+              <br />
+              <div className="well well-lg">
+                <strong>Hint:</strong> Let your interviewers know that
+                differentiating instruction is important and that you implement
+                strategies on a regular basis. All students
+                don".$single_quote."t learn in the same way, so it is important
+                to present ideas in more than one way. It is also important to
+                engage both gifted and struggling students. Think up a list of
+                ways you differentiate instruction and be prepared to share
+                actual examples.
+              </div>
+              <h3>Question #7:</h3>
+              <div>
+                <strong>{this.state.q7}</strong>
+              </div>
+              <br />
+              <div className="well well-lg">
+                <strong>Hint:</strong> Communication is a key part of education,
+                whether it be with a student, co-worker, administrator, or
+                parent. Be sure that you are regular communication with the
+                people around you. Have a specific describable plan to keep
+                parents "in the loop". Good communication with parents can be an
+                incredible force to get a child to behave and learn in the
+                classroom.
+              </div>
+              <h3>Question #8:</h3>
+              <div>
+                <strong>{this.state.q8}</strong>
+              </div>
+              <br />
+              <div className="well well-lg">
+                <strong>Hint:</strong> Always have a couple questions prepared
+                to ask at the end of the interview. Possible questions: "What is
+                it that YOU love about working here?" or "What originally
+                attracted you to work at this school?"
+              </div>
+              <div className="text-center">
+                <button
+                  className="btn btn-primary"
+                  onClick={this.generateQuestions}
+                >
+                  Generate a Random Set of Interview Questions!
+                </button>
+              </div>
             </div>
 
             {/* RIGHT SIDEBAR */}
