359d9de3a05b0d6241cd93c5c77722f6c886cfb8
Kevin Cauto <kevinmcauto@gmail.com>
2018-02-28 21:05:30 -0500

    The 'role selection' screen looks a lot nicer with images placed in.

--- client/src/components/RoleSelection.js
+++ client/src/components/RoleSelection.js
@@ -7,21 +7,41 @@ class RoleSelection extends Component {
   render() {
     return (
       <div className="container content-container">
-        <div className="col-md-6 extended">
-          <h2>Are you a teacher or employer?</h2>
+        <h2>Are you a teacher or employer?</h2>
+        <div className="extended">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="well">
+                <img
+                  src="/img/teacher.jpg"
+                  className="role-image"
+                  onClick={this.props.selectTeacher}
+                />
+                <br />
 
-          <button
-            onClick={this.props.selectTeacher}
-            className="btn-primary btn"
-          >
-            Teacher/Job Seeker
-          </button>
+                <button
+                  onClick={this.props.selectTeacher}
+                  className="btn-primary btn btn-block"
+                >
+                  Teacher/Job Seeker
+                </button>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="well">
+                <Link to="/recruiter/application">
+                  <img src="/img/recruiter.jpg" className="role-image" />
+                </Link>
+                <br />
 
-          <Link to="/recruiter/application">
-            <button className="btn-primary btn pull-right">
-              Employer/School District
-            </button>
-          </Link>
+                <Link to="/recruiter/application">
+                  <button className="btn-primary btn btn-block">
+                    Employer/School District
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
