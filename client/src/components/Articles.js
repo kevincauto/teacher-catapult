5d0d49fd3678bc4f35c85024d0ff5f31fbe99818
Kevin Cauto <kevinmcauto@gmail.com>
2018-03-10 23:33:27 -0500

    The articles page was given a sidebar and placeholder links.

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react';
 class Articles extends Component {
   render() {
     return (
-      <div className="container content-container">
+      <div className="content-container container ">
         <img src="../../img/mountain-background.jpeg" alt="bg" className="bg" />
-        <h2>Articles</h2>
+        <div className="row">
+          <div className="col-md-8 col-lg-9">
+            <h1>Articles</h1>
+            <ul>
+              <li><a href="#">Link1</a></li>
+              <li><a href="#">Link2</a></li>
+              <li><a href="#">Link3</a></li>
+              <li><a href="#">Link4</a></li>
+              <li><a href="#">Link5</a></li>
+              <li><a href="#">Link6</a></li>
+            </ul>
+          </div>
+          <RightSidebar />
+        </div>
       </div>
     );
   }
