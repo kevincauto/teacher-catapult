7dd6b047417cd0e6e7507c49a27d4eb8e39944b0
Kevin Cauto <kevinmcauto@gmail.com>
2019-03-24 01:06:51 -0400

    Added a redict in node backend to redirect to https.

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ require('./cron_jobs/crawlJobs');
 // require('./scripts/index.js');
 
 if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
+if (process.env.NODE_ENV === 'production') {
   //Express will serve up production assets
   //like our main.js file, or main.css file!
   app.use(express.static('client/build'));
